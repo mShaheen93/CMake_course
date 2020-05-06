402d5907c2679d67aafaa732a8b11e0d6de0bbc3
mshaheen <m.shaheen1993@gmail.com>
2020-05-06 02:44:54 +0200

    Add Library

---
+++ src/calculator/include/calculator.hpp
@@ -0,0 +1,5 @@
+
+
+int myAdd(int x,int y);
+
+int mySub(int x,int y);
---
+++ src/calculator/src/calculator.cpp
@@ -0,0 +1,11 @@
+#include "calculator.hpp"
+
+int myAdd(int x,int y)
+{
+    return x+y;
+}
+
+int mySub(int x,int y)
+{
+    return x-y;
+}
